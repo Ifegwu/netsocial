b20ea8cf92888b89e7ad93306f5a357b627d5085
Daniel Agbanyim <agbanyimdaniel29@gmail.com>
2023-03-08 16:03:26 +0100

    Program class house-keeping

--- API/Program.cs
+++ API/Program.cs
@@ -1,29 +1,16 @@
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistance;
 using Persistence;
-using MediatR;
-using Application.Activities;
-using Application.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddApplicationServices(builder.Configuration);
+
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlite(connectionString));
-
-builder.Services.AddCors(x => {
-    x.AddPolicy("CorsPolicy", policy => {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    });
-});
-
-builder.Services.AddMediatR(typeof(List.Handler).Assembly);
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
